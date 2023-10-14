95b7b0a9723a442a15b5e2e3841e5e894ca5e2f5
Medyacc <125936718+Medyacc@users.noreply.github.com>
2023-10-14 01:32:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let pageColor = document.querySelector(".pageColor");
 let colors = document.querySelectorAll(".color");
 let getImage = document.querySelectorAll(".projects-card.soon .project-image img");
+let imageProjet = document.querySelector(".projects-card .project-image img");
 
 
 if (window.localStorage.getItem("MainColor") && window.localStorage.getItem("SecondColor")) {
@@ -9,22 +10,25 @@ if (window.localStorage.getItem("MainColor") && window.localStorage.getItem("Sec
 
 if (window.localStorage.getItem("imageone") || window.localStorage.getItem("imagetwo") || window.localStorage.getItem("imagethree") || window.localStorage.getItem("imagefoor")) {
     if (document.styleSheets[1].rules[2].style.cssText == "--mainColor: #3a6cf4; --secondColor: #3a6cf4;") {
-        console.log(document.styleSheets[1].rules[2].style.cssText)
+        imageProjet.src = `images/${window.localStorage.getItem("imageone")}.jpg`;
         for (let i = 0; i < getImage.length; i++) {
             getImage[i].src = `images/${window.localStorage.getItem("imageone")}.png`;
         }
     }
     if (document.styleSheets[1].rules[2].style.cssText == "--mainColor: #D80032; --secondColor: #D80032;") {
+        imageProjet.src = `images/${window.localStorage.getItem("imagetwo")}.jpg`;
         for (let i = 0; i < getImage.length; i++) {
             getImage[i].src = `images/${window.localStorage.getItem("imagetwo")}.png`;
         }
     }
     if (document.styleSheets[1].rules[2].style.cssText == "--mainColor: #E55604; --secondColor: #E55604;") {
+        imageProjet.src = `images/${window.localStorage.getItem("imagethree")}.jpg`;
         for (let i = 0; i < getImage.length; i++) {
             getImage[i].src = `images/${window.localStorage.getItem("imagethree")}.png`;
         }
     }
     if (document.styleSheets[1].rules[2].style.cssText == "--mainColor: #419197; --secondColor: #419197;") {
+        imageProjet.src = `images/${window.localStorage.getItem("imagefoor")}.jpg`;
         for (let i = 0; i < getImage.length; i++) {
             getImage[i].src = `images/${window.localStorage.getItem("imagefoor")}.png`;
         }
@@ -41,24 +45,28 @@ colors.forEach(elm => {
     elm.addEventListener("click", function () {
         if (elm.classList.contains("one")) {
             document.styleSheets[1].rules[2].style.cssText = "--mainColor: #3a6cf4; --secondColor: #601cfc";
+            imageProjet.src = "images/one.jpg";
             for (let i = 0; i < getImage.length; i++) {
                 getImage[i].src = "images/one.png";
             }
         }
         if (elm.classList.contains("two")) {
             document.styleSheets[1].rules[2].style.cssText = "--mainColor: #D80032;--secondColor: #FF6969";
+            imageProjet.src = "images/two.jpg";
             for (let i = 0; i < getImage.length; i++) {
                 getImage[i].src = "images/two.png";
             }
         }
         if (elm.classList.contains("three")) {
             document.styleSheets[1].rules[2].style.cssText = "--mainColor: #E55604;--secondColor: #FFB000";
+            imageProjet.src = "images/three.jpg";
             for (let i = 0; i < getImage.length; i++) {
                 getImage[i].src = "images/three.png";
             }
         }
         if (elm.classList.contains("foor")) {
             document.styleSheets[1].rules[2].style.cssText = "--mainColor: #419197;--secondColor: #64CCC5";
+            imageProjet.src = "images/foor.jpg";
             for (let i = 0; i < getImage.length; i++) {
                 getImage[i].src = "images/foor.png";
             }
